7cdebf0c24e805ba401515246c0dd3ad050f86cf
Tomona Nanase <nanase@users.noreply.github.com>
2016-05-22 22:04:57 +0900

    add: IntervalにXMLドキュメントを追加

--- Synker/Interval.cs
+++ Synker/Interval.cs
@@ -5,6 +5,9 @@ using System.Threading;
 
 namespace Synker
 {
+    /// <summary>
+    /// 一定の間隔でイベントを発生させるための機能を提供します。
+    /// </summary>
     public class Interval
     {
         #region -- Private Fields --
@@ -16,6 +19,10 @@ namespace Synker
 
         #region -- Public Properties --
 
+        /// <summary>
+        /// イベントを発生する間隔値をミリ秒で設定または取得します。
+        /// 設定値は 1 ミリ秒以上の値でなくてはなりません。
+        /// </summary>
         public int IntervalMilliseconds
         {
             get { return intervalMilliseconds; }
@@ -28,20 +35,32 @@ namespace Synker
             }
         }
 
+        /// <summary>
+        /// タイマが動作しているかを表す真偽値を取得します。
+        /// </summary>
         public bool Running { get; private set; }
 
+        /// <summary>
+        /// イベントが発生した回数を取得します。
+        /// </summary>
         public long TickCount { get; private set; }
 
         #endregion
 
         #region -- Public Events --
 
+        /// <summary>
+        /// 指定された間隔が経過し、イベントを発生させようとしたときに発生します。
+        /// </summary>
         public event EventHandler<IntervalTickEventArgs> Elapsed;
 
         #endregion
 
         #region -- Constructors --
 
+        /// <summary>
+        /// パラメータを指定せずに新しい Interval クラスのインスタンスを指定します。
+        /// </summary>
         public Interval()
         {
             Reset();
@@ -51,6 +70,9 @@ namespace Synker
 
         #region -- Public Methods --
 
+        /// <summary>
+        /// タイマを動作させ、イベントを発生できる状態にします。
+        /// </summary>
         public void Start()
         {
             if (Running)
@@ -61,11 +83,18 @@ namespace Synker
             Running = true;
         }
 
+        /// <summary>
+        /// タイマを停止し、イベントの発生を止めます。
+        /// </summary>
         public void Stop()
         {
             Stop(TimeSpan.FromMilliseconds(-1));
         }
 
+        /// <summary>
+        /// タイマを停止し、イベントの発生を止め、指定された時間だけ停止を待ちます。
+        /// </summary>
+        /// <param name="timeout">停止待機時間を表す <see cref="TimeSpan"/> 型のインスタンス。</param>
         public void Stop(TimeSpan timeout)
         {
             if (!Running)
@@ -78,17 +107,27 @@ namespace Synker
             Running = false;
         }
 
+        /// <summary>
+        /// イベント発生カウントを初期化し、再び発生ができる状態にリセットします。
+        /// </summary>
         public void Reset()
         {
             TickCount = 0L;
             requestedStop = false;
         }
 
+        /// <summary>
+        /// タイマを停止し、再び開始します。
+        /// </summary>
         public void Restart()
         {
             Restart(TimeSpan.FromMilliseconds(-1));
         }
 
+        /// <summary>
+        /// 指定された時間だけ停止を待ち、タイマを再び開始します。
+        /// </summary>
+        /// <param name="timeout">停止待機時間を表す <see cref="TimeSpan"/> 型のインスタンス。</param>
         public void Restart(TimeSpan timeout)
         {
             Stop(timeout);
