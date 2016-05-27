e885dbf465741c38435f708649e23c6be153271a
Tomona Nanase <nanase@users.noreply.github.com>
2016-05-27 23:53:52 +0900

    Add: TimerElapsedEventArgsTestを追加

---
+++ test/UnitTest/TimerElapsedEventArgsTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Synker;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TimerElapsedEventArgsTest
+    {
+        [TestMethod]
+        public void IntervalGapTest()
+        {
+            var args = new TimerElapsedEventArgs(1, 0, 0, 0);
+            Assert.AreEqual(0L, args.IntervalGapTick);
+            Assert.AreEqual(0.0, args.IntervalGapTime);
+        }
+
+        [TestMethod]
+        public void LastEventProcessTest()
+        {
+            var args = new TimerElapsedEventArgs(1, 0, 0, 0);
+            Assert.AreEqual(0L, args.LastEventProcessTick);
+            Assert.AreEqual(0.0, args.LastEventProcessTime);
+        }
+    }
+}
