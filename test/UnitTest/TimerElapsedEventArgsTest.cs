46555f12b1ce4efad0f1479622fda9637d920910
Tomona Nanase <nanase@users.noreply.github.com>
2016-05-31 00:00:08 +0900

    update: TimerElapsedEventArgsTestを更新

--- test/UnitTest/TimerElapsedEventArgsTest.cs
+++ test/UnitTest/TimerElapsedEventArgsTest.cs
@@ -9,6 +9,15 @@ namespace UnitTest
     public class TimerElapsedEventArgsTest
     {
         [TestMethod]
+        public void CtorError()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            ExceptionAssert.Expect(typeof(ArgumentOutOfRangeException), () => new TimerElapsedEventArgs(0, 0, 0, 0));
+            ExceptionAssert.Expect(typeof(ArgumentOutOfRangeException), () => new TimerElapsedEventArgs(1, -1, 0, 0));
+            ExceptionAssert.Expect(typeof(ArgumentOutOfRangeException), () => new TimerElapsedEventArgs(1, 0, 0, -1));
+        }
+
+        [TestMethod]
         public void IntervalGapTest()
         {
             var args = new TimerElapsedEventArgs(1, 0, 0, 0);
