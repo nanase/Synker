db431aa37e36a0ef62feedb4695e000e727d4489
Tomona Nanase <nanase@users.noreply.github.com>
2016-06-12 21:47:31 +0900

    add: TimedOutEventArgsTestを追加

---
+++ test/UnitTest/TimedOutEventArgsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Synker;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TimedOutEventArgsTest
+    {
+        [TestMethod]
+        [SuppressMessage("ReSharper", "ObjectCreationAsStatement")]
+        public void CtorError()
+        {
+            ExceptionAssert.Expect(typeof(ArgumentOutOfRangeException), () => new TimedOutEventArgs(0, 42L));
+        }
+
+        [TestMethod]
+        public void IntervalGapTest()
+        {
+            var args = new TimedOutEventArgs(1, 42);
+            Assert.AreEqual(42L, args.IntervalGapTick);
+            Assert.AreEqual(42.0, args.IntervalGapTime);
+        }
+    }
+}
